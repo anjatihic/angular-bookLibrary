e8b7581524c362e4096af82d373cac2eba8a00a6
Anja Tihic <atihic99@gmail.com>
2023-02-13 17:08:58 +0100

    Add validation for books and authors

--- src/app/create-new/new-author/new-author.component.ts
+++ src/app/create-new/new-author/new-author.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {Author} from "../../models/author.model";
 import {AuthorService} from "../../shared/services/author.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-new-author',
@@ -11,9 +12,10 @@ export class NewAuthorComponent {
 
   constructor(private authorService: AuthorService) {
   }
-  onCreateAuthor(authorData: Author){
+  onCreateAuthor(authorData: Author, authorForm: NgForm){
     console.log('onCreateAuthor()')
     this.authorService.createAuthor(authorData.name, authorData.surname, authorData.yearBorn, authorData.yearDied);
     alert('Author is created');
+    authorForm.reset();
   }
 }
